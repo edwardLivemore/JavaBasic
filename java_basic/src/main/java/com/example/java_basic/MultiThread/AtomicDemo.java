a0323205c3e2171423e698964509b8e1e3d09fa9
zhanxiang <zhanxiang@chinadep.com>
2022-09-25 21:05:13 +0800

    AtomicInteger Demo

--- java_basic/src/main/java/com/example/java_basic/MultiThread/AtomicDemo.java
+++ java_basic/src/main/java/com/example/java_basic/MultiThread/AtomicDemo.java
@@ -1,24 +1,43 @@
 package com.example.java_basic.MultiThread;
 
-import java.util.HashSet;
-import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Slf4j
 public class AtomicDemo {
     // plan A
     AtomicInteger count = new AtomicInteger(0);
-    Set<Integer> set = new HashSet<>();
+    List<Integer> list = new ArrayList<>();
+    Vector<Integer> vector = new Vector<>();
+    CopyOnWriteArrayList<Integer> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
 
     public void addToTotal() {
-        while (count.get() < 1000) {
-//            System.out.println(i);
-            int value = count.incrementAndGet();
-            set.add(value);
-            System.out.println(Thread.currentThread().getName() + ":" + value);
-//            if (value == total) {
-//                System.out.println("end");
-//            }
+//        while (count.get() < 1000) {
+////            System.out.println(i);
+//            int value = count.incrementAndGet();
+//            list.add(value);
+//            System.out.println(Thread.currentThread().getName() + ":" + value);
+////            if (value == total) {
+////                System.out.println("end");
+////            }
+//        }
+
+        while (true) {
+            if (count.incrementAndGet() <= 100000) {
+                int value = count.get();
+//                log.info("{}", value);
+//                list.add(value);
+//                vector.add(value);
+                copyOnWriteArrayList.add(value);
+            } else {
+                break;
+            }
         }
     }
 
@@ -38,10 +57,11 @@ public class AtomicDemo {
     public static void main(String[] args) {
         AtomicDemo atomicDemo = new AtomicDemo();
 
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(8, 8,
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 20,
                 60, TimeUnit.SECONDS,
                 new LinkedBlockingDeque<>(10), new ThreadPoolExecutor.AbortPolicy());
 
+        log.info("start...");
         long startTime = System.currentTimeMillis();
         int i = 0;
         while (i++ < 10) {
@@ -49,9 +69,19 @@ public class AtomicDemo {
         }
         executor.shutdown();
         while (!executor.isTerminated()) ;
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
         long endTime = System.currentTimeMillis();
         System.out.println("duration : " + (endTime - startTime) + "ms");
         System.out.println("count = " + atomicDemo.count.get());
-        System.out.println("set size = " + atomicDemo.set.size());
+//        System.out.println("list size = " + atomicDemo.list.size());
+//        System.out.println("vector size = " + atomicDemo.vector.size());
+//        System.out.println("vector max value = " + Collections.max(atomicDemo.vector));
+        System.out.println("copyOnWriteArrayList size = " + atomicDemo.copyOnWriteArrayList.size());
+        System.out.println("copyOnWriteArrayList max value = " + Collections.max(atomicDemo.copyOnWriteArrayList));
+        log.info("end");
     }
 }
